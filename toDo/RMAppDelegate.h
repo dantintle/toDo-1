e8a345087131edfbe3ee3f0220823a3448070cdd
Robert Manson <robmanson@gmail.com>
2013-10-13 17:57:59 -0700

    Initial Commit

---
+++ toDo/RMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RMAppDelegate.h
+//  toDo
+//
+//  Created by Robert Manson on 10/13/13.
+//  Copyright (c) 2013 Robert Manson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
